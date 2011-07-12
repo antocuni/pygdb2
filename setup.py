9b0cbb7bbc0bea9189bf768af66085d29efde8e3
Antonio Cuni <anto.cuni@gmail.com>
2011-07-12 15:17:48 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name = "pygdb2",
+    version = "0.1",
+    author = "Antonio Cuni",
+    author_email = "anto.cuni@gmail.com",
+    url = "https://bitbucket.org/antocuni/pygdb2/",
+    license = "BSD",
+    platforms = ["unix", "linux"],
+    packages = ["pygdb2"],
+    description = "Control GDB from within the Python program being debugged",
+    long_description = "Control GDB from within the Python program being debugged",
+    )
